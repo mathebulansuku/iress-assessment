d4483aee968df7541ee3fbf28f4033469cbe5041
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-10-29 00:01:09 +0200

    Outputs: add api_url, frontend bucket and URL

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,18 @@ output "tf_state_bucket_name" {
 output "tf_lock_table_name" {
   value = aws_dynamodb_table.tf_lock.name
 }
+
+output "api_url" {
+  description = "Base API URL"
+  value       = coalesce(module.api_gateway.invoke_url, module.api_gateway.api_endpoint)
+}
+
+output "frontend_bucket" {
+  description = "Frontend website S3 bucket"
+  value       = module.frontend_website.bucket_name
+}
+
+output "frontend_url" {
+  description = "Frontend website URL (S3 website endpoint)"
+  value       = module.frontend_website.website_endpoint
+}
